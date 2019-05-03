10e83328b486e576b5d97ad8024fc28d554b20ec
j.c.sackett <j.c.sackett@gmail.com>
2019-05-03 14:53:12 +0000

    Cleans up setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup
 
-setup(name='txroutes',
-        version='0.1.2',
-        author='Olark',
-        url='http://github.com/olark/txroutes',
-        description='Provides routes-like dispatching for twisted.web.server',
-        long_description='''Frequently, it's much easier to describe your website layout using routes instead of Resource from twisted.web.resource. This small library lets you dispatch with routes in your twisted.web application.''',
-        keywords='twisted, web, routes',
-        classifiers=['Programming Language :: Python', 'License :: OSI Approved :: BSD License'],
-        license='BSD',
-        packages=['txroutes'],
-        package_data={'txroutes': ['*']},
-        install_requires=['routes', 'twisted'],
-        )
+LONG_DESCRIPTION = '''
+Frequently, it's much easier to describe your website layout using routes
+instead of Resource from twisted.web.resource. This small library lets you
+dispatch with routes in your twisted.web application.
+'''
+
+setup(
+    name='txroutes',
+    version='0.1.2',
+    author='Olark',
+    url='http://github.com/olark/txroutes',
+    description='Provides routes-like dispatching for twisted.web.server',
+    long_description=LONG_DESCRIPTION,
+    keywords='twisted, web, routes',
+    classifiers=['Programming Language :: Python', 'License :: OSI Approved :: BSD License'],
+    license='BSD',
+    packages=['txroutes'],
+    package_data={'txroutes': ['*']},
+    install_requires=['routes', 'twisted'],
+)
 
